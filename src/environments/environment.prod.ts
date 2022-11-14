3a182d4cca2dae14d5e7f5525d500934afb0caab
Urias <uriass.perez@gmail.com>
2022-11-14 14:10:14 -0500

    cambios en enviroments.pro.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:  {
+    apiKey: "AIzaSyC6UAjqNXgr0hF0PC_eFu87SQ2QfrEeHx4",
+    authDomain: "login-app-42c2a.firebaseapp.com",
+    projectId: "login-app-42c2a",
+    storageBucket: "login-app-42c2a.appspot.com",
+    messagingSenderId: "880617289253",
+    appId: "1:880617289253:web:7f237f7a90c83a6a48f1cc",
+    measurementId: "G-DWVH920PDJ"
+  }
 };
