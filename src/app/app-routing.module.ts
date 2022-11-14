997ed5c2678b5aea1389d738e29301ba9639b61d
Urias_Dev <uriass.perez@gmail.com>
2022-11-13 21:46:57 -0500

    v1 login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const  routes: Routes = [
+  {
+     path:'',
+     redirectTo: 'login' ,
+      pathMatch: 'full'
+  },
+
+  {
+    path: 'login' , loadChildren: ()=>import('./login/login.module' ).then(m => m.LoginModule )
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports:  [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'   ;
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import  { AppComponent } from './app.component';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations:  [
+     AppComponent
   ],
-  imports: [
+     imports:    [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
-  providers: [],
+  providers: [
+   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
